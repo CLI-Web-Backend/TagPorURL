88b1e0bee3fa7233f26234d4837080f14cca7ab4
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2026-05-11 12:48:24 -0300

    Adiciona suporte a dependências e implementa parser HTML com DiDOM

--- src/index.php
+++ src/index.php
@@ -1,1 +1,13 @@
-echo("Hello, World!");
+<?php
+require "vendor/autoload.php";
+
+use DiDom\Document;
+
+$document = new Document('https://www.mercadolivre.com.br/', true);
+
+$posts = $document->find('title');
+
+foreach($posts as $post) {
+    echo $post->text(), "\n";
+}
+?>
